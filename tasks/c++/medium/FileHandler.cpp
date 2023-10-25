371fdb0d946b20bf11cd2a9ea4caa1199f942d97
Kunal Sharma <cse.190320027@silicon.ac.in>
2023-10-25 21:35:57 +0530

    Update FileHandler.cpp

--- tasks/c++/medium/FileHandler.cpp
+++ tasks/c++/medium/FileHandler.cpp
@@ -1,14 +1,55 @@
-// C++ - Medium
-
 #include "FileHandler.h"
 #include <fstream>
-#include <sstream>
+#include <iostream>
 
-// TODO: Implement the FileHandler::ReadFile method
+FileHandler::FileHandler() {}
 
-// TODO: Implement the FileHandler::WriteFile method
+FileHandler::~FileHandler() {}
 
-// TODO: Implement the FileHandler::AppendFile method
+std::string FileHandler::ReadFile(const std::string& filePath) {
+    std::string content;
+    std::ifstream file(filePath);
 
-// TODO: Implement the FileHandler::FileExists method
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            content += line + "\n";
+        }
+        file.close();
+    } else {
+        std::cerr << "Failed to open the file for reading: " << filePath << std::endl;
+    }
 
+    return content;
+}
+
+bool FileHandler::WriteFile(const std::string& filePath, const std::string& content) {
+    std::ofstream file(filePath);
+
+    if (file.is_open()) {
+        file << content;
+        file.close();
+        return true;
+    } else {
+        std::cerr << "Failed to open the file for writing: " << filePath << std::endl;
+        return false;
+    }
+}
+
+bool FileHandler::AppendFile(const std::string& filePath, const std::string& content) {
+    std::ofstream file(filePath, std::ios::app);
+
+    if (file.is_open()) {
+        file << content;
+        file.close();
+        return true;
+    } else {
+        std::cerr << "Failed to open the file for appending: " << filePath << std::endl;
+        return false;
+    }
+}
+
+bool FileHandler::FileExists(const std::string& filePath) {
+    std::ifstream file(filePath);
+    return file.good();
+}
